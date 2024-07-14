61cb6114c2900ba67bf5ef38483ad29e287ab946
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-14 10:50:24 +0300

    added image upload.

--- app/Livewire/Posts.php
+++ app/Livewire/Posts.php
@@ -5,14 +5,17 @@ namespace App\Livewire;
 use App\Models\Post;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Posts extends Component
 {
+    use WithFileUploads;
 
     #[Validate("required|min:5|max:50")]
     public $title;
     #[Validate("required|min:5|max:250")]
     public $content;
+    #[Validate("nullable|image|max:2048")]
     public $image;
 
     public function create()
@@ -20,7 +23,8 @@ class Posts extends Component
         $this->validate();
         Post::create([
             "title" => $this->title,
-            "content" => $this->content
+            "content" => $this->content,
+            "image" => $this->image->store("uploads/posts", "public"),
         ]);
         $this->reset();
     }
