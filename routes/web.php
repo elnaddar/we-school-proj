b637528de060d6625ef22d0ef30ccb6ddb8bfc0b
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-14 10:15:45 +0300

    create posts component.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Livewire\Counter;
+use App\Livewire\Posts;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/counter', Counter::class)->name('counter');
+Route::get('/posts', Posts::class)->name('posts');
