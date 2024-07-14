3576b1b35fd30402860315b5b9ad81f24d53da8c
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-14 10:13:45 +0300

    make layout and route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/counter', Counter::class)->name('counter');
