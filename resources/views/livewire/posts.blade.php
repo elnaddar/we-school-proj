6905b91bb516fa6691abcd7c2d34d0aa65aa0120
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-14 10:21:50 +0300

    made posts view

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -1,3 +1,18 @@
 <div>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <form>
+        <p>
+            <label for="title">Title: </label>
+            <input id="title" type="text">
+        </p>
+        <p>
+            <label for="image">Image: </label>
+            <input id="image" type="file" accept="image/png image/jpeg">
+        </p>
+        <p>
+            <label for="content">Content: </label>
+            <textarea id="content"></textarea>
+        </p>
+        <input type="submit" value="Create Post">
+    </form>
 </div>
