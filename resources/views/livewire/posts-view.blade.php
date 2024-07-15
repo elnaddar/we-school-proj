5bd9a5411db1319d801af61f1046d1a5b8cc8da7
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-15 10:46:10 +0300

    added style

--- resources/views/livewire/posts-view.blade.php
+++ resources/views/livewire/posts-view.blade.php
@@ -1,13 +1,23 @@
-<div>
+<div class="items-start lg:flex">
     {{-- Nothing in the world is as soft and yielding as water. --}}
     @foreach ($posts as $post)
-        <div class="flex items-center">
+        <div class="max-w-sm mb-2 overflow-hidden bg-white rounded shadow-lg lg:mr-2">
             @if ($post->image)
-                <img src="{{ asset('storage/' . $post->image) }}" class="w-10 h-10 mr-1 rounded">
+                <img class="w-full" src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->title }}">
             @endif
-            <div>
-                <h2 class="font-bold">{{ $post->title }}</h2>
-                <p>{{ $post->content }}</p>
+            <div class="px-6 py-4">
+                <div class="mb-2 text-xl font-bold">{{ $post->title }}</div>
+                <p class="text-base text-gray-700">
+                    {{ $post->content }}
+                </p>
+            </div>
+            <div class="px-6 pt-4 pb-2">
+                <span
+                    class="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">#photography</span>
+                <span
+                    class="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">#travel</span>
+                <span
+                    class="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">#winter</span>
             </div>
         </div>
     @endforeach
