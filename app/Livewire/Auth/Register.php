4f663e231209dc919603ee4ece37ad8b4c10a80b
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-27 19:47:55 +0300

    constructed register component

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -2,10 +2,28 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Register extends Component
 {
+    #[Validate("required|min:5|max:42")]
+    public $name;
+    #[Validate("required|email")]
+    public $email;
+    #[Validate("required|confirmed")]
+    public $password;
+    public $password_confirmation;
+
+    public function register(){
+        $this->validate();
+        User::create([
+            "name"=> $this->name,
+            "email"=> $this->email,
+            "password"=> $this->password,
+        ]);
+    }
     public function render()
     {
         return view('livewire.auth.register');
