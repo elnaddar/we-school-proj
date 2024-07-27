dd1179c243a39875507631ad96f0221057f0b68c
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-27 19:56:08 +0300

    added routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Livewire\Auth\Login;
+use App\Livewire\Auth\Register;
 use App\Livewire\Counter;
 use App\Livewire\PostsPage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/counter', Counter::class)->name('counter');
-Route::get('/posts', PostsPage::class)->name('posts');
+Route::middleware("auth")->group(function () {
+    Route::get('/counter', Counter::class)->name('counter');
+    Route::get('/posts', PostsPage::class)->name('posts');
+});
+Route::middleware("guest")->group(function () {
+    Route::get("/login", Login::class)->name("login");
+    Route::get("/register", Register::class)->name("register");
+});
