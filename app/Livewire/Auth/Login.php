4645e477007f48934b9fe3d0237fe1e5a37bbb84
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-07-28 09:54:02 +0300

    added login

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -2,10 +2,23 @@
 
 namespace App\Livewire\Auth;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Login extends Component
 {
+    #[Validate("required|email")]
+    public $email;
+    #[Validate("required")]
+    public $password;
+    public bool $remember = false;
+
+    public function login(){
+        $this->validate();
+        if(auth()->attempt(["email"=> $this->email,"password"=> $this->password], $this->remember)){
+            redirect()->route("home");
+        }
+    }
     public function render()
     {
         return view('livewire.auth.login');
